0df149ea73346540e9ee7b025d8ad70d323bd181
Stuart Mason <stuart@sunboundhomes.com>
2025-01-20 14:45:10 -0500

    r gitignores and trailing spaces

--- tests/test_regexes.py
+++ tests/test_regexes.py
@@ -4,6 +4,9 @@ from main import DIGIT_ONLY, NAME_SPACE_DIGIT
 def test_digit_only():
     regex = re.compile(DIGIT_ONLY)
     assert regex.match('1')
+    assert regex.match('1 ')
+    assert regex.match('1  ')
+    assert not regex.match(' 1  ')
     assert not regex.match('-1')
     assert not regex.match('0.5')
     assert not regex.match(' 1')
@@ -12,7 +15,11 @@ def test_digit_only():
 def test_name_space_digit():
     regex = re.compile(NAME_SPACE_DIGIT)
     assert regex.match('Name 1')
-    assert regex.match('Name 2')
+    assert regex.match('Name 20')
+    assert regex.match('Name 1 ')
+    assert regex.match('Name 1  ')
+    assert not regex.match(' Name 1  ')
+    assert not regex.match(' Name 1')
     assert not regex.match('This is 2')
     assert not regex.match('Name only')
     assert not regex.match('Name -1')
